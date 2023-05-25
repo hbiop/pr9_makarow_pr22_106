c07c966a9e197e0c257678055a6f4a2bc89f7eb3
starterov <starterror@duck.com>
2023-05-25 09:14:52 +0700

    2

--- app/src/main/java/com/example/pz9_makarow_pr22_106/MainActivity.java
+++ app/src/main/java/com/example/pz9_makarow_pr22_106/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.pz9_makarow_pr22_106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sw = (Button)findViewById(R.id.button2);
+        sw.setOnClickListener(this);
+    }
+    public void onClick(View view)
+    {
+        Intent in = new Intent(this, MainActivity2.class);
+        startActivity(in);
     }
 }
--- app/src/main/java/com/example/pz9_makarow_pr22_106/MainActivity2.java
+++ app/src/main/java/com/example/pz9_makarow_pr22_106/MainActivity2.java
@@ -2,13 +2,25 @@ package com.example.pz9_makarow_pr22_106;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity2 extends AppCompatActivity {
-
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+Button onMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        onMap = (Button) findViewById(R.id.button7);
+        onMap.setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View view)
+    {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("geo: 55.03348867, 82.91666678")));
     }
 }
