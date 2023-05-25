33296ef420625c1387bcebe7de17ad4d8208f676
starterov <starterror@duck.com>
2023-05-25 09:15:59 +0700

    3

--- app/src/main/java/com/example/pz9_makarow_pr22_106/MainActivity.java
+++ app/src/main/java/com/example/pz9_makarow_pr22_106/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sw = (Button)findViewById(R.id.button2);
+        sw = (Button)findViewById(R.id.button4);
         sw.setOnClickListener(this);
     }
     public void onClick(View view)
